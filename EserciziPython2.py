083fa11d080ff52740ead3a922ca17aca0854b7d
cri2725 <cristian.istrati27@gmail.com>
2025-06-25 20:39:01 +0200

    boba

--- EserciziPython2.py
+++ EserciziPython2.py
@@ -42,3 +42,64 @@ def sumCSV(filepath):
 file="/home/cri2725/programmazione/LaboProg/CSVfiles/shampoo_sales.csv"
 print(f"la somma è {sumCSV(file)}")
 """
+"""
+1. Definire una funzione che prende in input un file ed una parola e conta quante volte
+quella parola è presente sul file
+"""
+"""
+def contaparole(fileconta, parola):
+    contatore=0
+    with open(fileconta, "r") as files:
+        bob=files.read()
+        elementi=bob.split()
+        for elemento in elementi:
+                if elemento == parola:
+                    contatore+=1
+    return contatore
+parola="\n"
+print(f"il num di volte che {parola} è stato usato è {contaparole("/home/cri2725/programmazione/LaboProg/CSVfiles/testoProva.txt", parola)}")
+"""
+        
+"""
+2. Definire una funzione che prende come input un file e conta quante volte ogni parola è
+presente
+"""
+"""
+def contpar(filepath):
+    panum=[]
+    with open(filepath, "r") as file:
+        fileStringa=file.read()
+        parole=fileStringa.split()
+        for parola in parole:
+            if parola not in panum:
+                panum.append(parola)
+                panum.append(1)
+            else:
+                for i,par in enumerate(panum):
+                    if par==parola:
+                        panum[i+1]+=1
+    return panum
+file="/home/cri2725/programmazione/LaboProg/CSVfiles/testoProva.txt"
+print(f"{contpar(file)}")
+"""
+"""
+3. Definire una funzione che prende in input un file e costruisce un dizionario con chiavi la
+lettere iniziali e con valore le parole di lunghezza maggiore contenute nel file che
+iniziano con quelle lettere.
+"""
+def dizion(filepath):
+    dizionario={}
+    with open(filepath, "r") as file:
+        fileStringa=file.read()
+        parole=fileStringa.split()
+        
+        
+        
+"""
+4. Definire una funzione conteggio che prende come input un file e ritorna un dizionario
+con chiave la prima parola di ogni frase e valore il numero di volte che una frase inizia
+con quella parola. Considerare come inizio di frase qualsiasi parola che segue un
+punto, un punto esclamativo, un punto interrogativo o si trova all'inizio del testo.
+5. Definire una funzione che prende come input un file, rimuove tutte le righe duplicate,
+scrive il risultato in un nuovo file chiamato unique.txt
+"""
